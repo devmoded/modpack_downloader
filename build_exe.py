0ab0aa599b70026803760837a480f7c582fce5ef
devmoded <matveyykas@gmail.com>
2026-04-02 20:28:33 +0300

    feat: обновление скрипта сборки

--- build_exe.py
+++ build_exe.py
@@ -1,10 +1,29 @@
 import subprocess
 from pathlib import Path
-# import sys
+from sys import platform
+from platformdirs import user_data_path
 
+print('Сборка исполняемого файла')
 subprocess.run([
     'pyinstaller', '--noconfirm', '--onefile', '--windowed',
     '--name', 'modpack-downloader',
     '--paths', Path('src'),
     Path('src/modpack_downloader/main.py')
 ])
+print('✅️ Сборка исполняемого файла завершена')
+
+if platform.startswith('win'):
+    iscc_path = user_data_path() / 'Programs' / 'Inno Setup 6' / 'ISCC.exe'
+    if iscc_path.exists() and iscc_path.is_file():
+        print('Сборка setup-файла через Inno Setup')
+        compile_result = subprocess.run(
+            [iscc_path, Path('setup.iss')],
+            capture_output=True,
+            text=True
+        )
+        print(compile_result.stdout)
+        if compile_result.returncode != 0:
+            print('❌️ Во время сборки setup-файла произошла ошибка')
+            print(compile_result.stderr)
+        else:
+            print('✅️ Сборка setup-файла через Inno Setup завершена')
